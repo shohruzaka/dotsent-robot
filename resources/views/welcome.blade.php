2801b2b4601e3f2801547945ad8ea582171f8b85
Shohruz <tshohruz@gmail.com>
2024-11-06 12:28:24 +0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dotsent robot - Telegram bot</title>
+</head>
+<body>
+    <h4>Dotsent bot</h4>
+</body>
+</html>
